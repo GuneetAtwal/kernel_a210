abe2c2c5b82039f6409ee4cabe3abea308e26aa2
GuneetAtwal <harjinder767@gmail.com>
2014-03-25 19:35:09 +0530

    drivers : binder.c avoid binder crash and getting rid of MTK Suff This aligns binder with kernel-common

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 8 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#8 SMP PREEMPT Mon Mar 24 17:13:28 IST 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Mar 24 19:33:04 IST 2014"
 #define LINUX_COMPILE_BY "guneetatwal"
 #define LINUX_COMPILE_HOST "guneetatwal-desktop"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- kernel/include/linux/version.h
+++ kernel/include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 197683
+#define LINUX_VERSION_CODE 197637
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
